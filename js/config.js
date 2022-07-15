ee8f7ab89afa4aa482298d1f88daae8d458b4ddc
James Chadband <james@chadband.me.uk>
2022-07-15 16:53:31 +0100

    Updated config for API Gateway

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-2_zz19xPA2T', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '158nqkil5utrbfah5l68k5529r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-2' // e.g. us-east-2
+        region: 'eu-west-2  ' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://s1ho3u8b52.execute-api.eu-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
